e5f353e5842c9f885f7bb4acb0484799da3cf886
Divanshusa <141446108+Divanshusa@users.noreply.github.com>
2023-08-29 15:15:19 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ if [ "$?" -eq 0 ]
 then
 docker stop web
 docker rm web
+docker build -t img2 .
+docker run -dit -p 8081:80 --name web img2
 else 
 docker build -t img2 .
 docker run -dit -p 8081:80 --name web img2
