ced24aa8d47db788a348e5cdb3d9a8fac9130ef8
Divanshusa <141446108+Divanshusa@users.noreply.github.com>
2023-09-01 01:35:47 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ then
 docker stop NGX
 docker rm NGX
 docker build -t img3 .
-docker run -dit -p 8081:80 --name NGX img3
+docker run -dit -p 8082:80 --name NGX img3
 else 
 docker build -t imG3 .
-docker run -dit -p 8081:80 --name NGX img3
+docker run -dit -p 8082:80 --name NGX img3
 fi
